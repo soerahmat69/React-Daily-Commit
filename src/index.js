6e7b51acd6346d8002b10608cb252784c984f7ef
soerahmat69 <surahmate38@gmail.com>
2023-06-09 03:35:32 +0800

    upd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Porto';
+import Porto from './Porto/Porto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <App/>
+        <Porto/>
   </React.StrictMode>
 );
