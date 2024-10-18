ecf4e54c6013c81dd7c40a167082828a15ef471f
soerahmat69 <surahmatr38@gmail.com>
2024-10-19 05:43:34 +0800

    update

--- src/Porto/PortoSec.jsx
+++ src/Porto/PortoSec.jsx
@@ -24,7 +24,28 @@ function PortoSec() {
       },
     },
   };
+  const listSkill = {
+    open: {
+      opacity: 1, 
+    
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.5,
+        delayChildren: 1
+      },
+    },
+    closed: {
+      opacity: 0,  
+      transition: {
+        when: "afterChildren",
+        staggerChildren: 0.5,
+        delayChildren: 1
+      },
+    },
+  }
   const item = { open: { y: -15, opacity: 1 }, close: { y: 15, opacity: 0 } };
+  const itemSkill = {    open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: -100 }, };
   const refclip = useRef(false);
   const refclip1 = useRef(false);
   const refclip2 = useRef(false);
@@ -53,14 +74,15 @@ function PortoSec() {
   const experience = [
     {
       position: "It Support",
-      institution:"Poltekkes Kemenkes Jakarta 3",
-      duration_job:"14 Agu 2023 - 14 Dec 2024",
-      as_work_position:"intern",
-      do_work:["Manage website and make loan vehicle development",
-        "Problem solving hardware and computer"
-      ]
-    }
-  ]
+      institution: "Poltekkes Kemenkes Jakarta 3",
+      duration_job: "14 Agu 2023 - 14 Dec 2024",
+      as_work_position: "intern",
+      do_work: [
+        "Manage website and make loan vehicle development",
+        "Problem solving hardware and computer",
+      ],
+    },
+  ];
   const clipBird = () => {
     let currentClip = b1;
     let currentClip1 = b1;
@@ -76,7 +98,7 @@ function PortoSec() {
     setInterval(() => {
       refclip2.current.style.backgroundImage = `url(${currentClip})`;
       currentClip2 = currentClip2 === b1 ? b2 : b1;
-    }, 100); 
+    }, 100);
   };
   const clipBirds = () => {
     let currentClip = b1;
@@ -91,7 +113,7 @@ function PortoSec() {
   }, []);
 
   return (
-    <div className=" overflow-x-hidden">
+    <main className=" overflow-x-hidden lg:mx-auto">
       <section className="text-center text-[#535353] h-screen w-screen flex flex-col justify-center">
         <motion.ul animate="close" variants={list}>
           <motion.li
@@ -129,28 +151,29 @@ function PortoSec() {
           initial={{ y: 70, opacity: 0 }}
           whileInView={{ y: -70, opacity: 1 }}
           transition={{ ease: "easeInOut", duration: 0.6 }}
-          className="font-['vt323'] -mb-[70px] text-[93px] uppercase tracking-[0.2em]"
+          className="font-['Press_Start_2P'] -mb-[60px] text-[3.5rem] uppercase tracking-[0.2em]"
         >
           Fullstack Web Dev.
         </motion.h1>
-        <motion.h2
+        <motion.p
           initial={{ y: 10, opacity: 0 }}
           whileInView={{ y: -10, opacity: 1 }}
           transition={{ ease: "easeInOut", duration: 0.6 }}
-          className="font-['vt323'] -mt-5 text-[43px] uppercase tracking-[0.2em]"
+          className="font-['Press_Start_2P']  text-[2rem] font-light uppercase tracking-[0.2em]"
         >
           Surahmat
-        </motion.h2>
+        </motion.p>
 
         <motion.h2
           initial={{ y: -10, opacity: 0 }}
           whileInView={{ y: 10, opacity: 1 }}
           transition={{ ease: "easeInOut", duration: 0.6 }}
-          className="font-['vt323'] text-[23px] -mt-4 cursor-pointer underline tracking-[0.2em]"
+          className="font-['Press_Start_2P'] text-[1rem] -mt-4 cursor-pointer underline tracking-[0.2em]"
         >
           Download CV
         </motion.h2>
       </section>
+
       <section className="text-center my-10 text-[#535353] h-screen w-screen flex flex-col justify-center">
         <motion.ul animate="close" className="relative" variants={list}>
           <motion.li
@@ -161,22 +184,22 @@ function PortoSec() {
             style={{ backgroundImage: `url(${c1})` }}
             className="bg-cover bg-center  w-[60px] h-[140px]  absolute top-20 left-20"
           />
-     
-           <motion.li
+
+          <motion.li
             variants={item}
             transition={{ ease: "easeInOut", duration: 0.8 }}
             whileInView={"open"}
             ref={refclip1}
             className="bg-cover bg-center  w-[80px] h-[90px]  absolute  -inset-[200px]  mx-auto"
           />
-              <motion.li
+          <motion.li
             variants={item}
             transition={{ ease: "easeInOut", duration: 0.8 }}
             whileInView={"open"}
             ref={refclip}
             className="bg-cover bg-center  w-[80px] h-[90px]  absolute  -inset-[250px] left-10 mx-auto"
           />
-           <motion.li
+          <motion.li
             variants={item}
             transition={{ ease: "easeInOut", duration: 0.8 }}
             whileInView={"open"}
@@ -190,7 +213,7 @@ function PortoSec() {
             style={{ backgroundImage: `url(${c2})` }}
             className="bg-cover bg-center  w-[50px] h-[120px]  absolute bottom-[180px] left-[220px]"
           />
-          
+
           <motion.li
             variants={item}
             transition={{ ease: "easeInOut", duration: 0.8 }}
@@ -200,38 +223,64 @@ function PortoSec() {
           />
         </motion.ul>
 
-        <ul className="flex mx-auto gap-4  ">
+        <motion.ul
+        initial="closed"
+        whileInView="open"
+                
+          variants={listSkill}
+ 
+          className="flex mx-auto gap-4  "
+        >
           {skill.map((result) => (
-            <li className=" ring-2 ring-[#535353]   rounded-[21px] w-[130px]  ">
+            <motion.li
+        
+            variants={itemSkill}
+          
+        
+              style={{ borderColor: "#535353" }}
+              className=" nes-container   w-[190px] with-title  "
+            >
+              <p className=" title font-['Press_Start_2P']  text-center text-[0.9rem] ">
+                {result.value}%
+              </p>
               <img className="w-full" src={result.img} alt="js" srcset="" />
-              <p className="font-['vt323'] font-bold py-4 text-[2rem]">
+              <p className=" font-bold font-['Press_Start_2P']  text-center text-[0.9rem] ">
                 {result.name}
               </p>
-            </li>
+            </motion.li>
           ))}
-        </ul>
+          
+        </motion.ul>
 
         <motion.h1
           initial={{ y: 70, opacity: 0 }}
           whileInView={{ y: -70, opacity: 1 }}
           transition={{ ease: "easeInOut", duration: 0.6 }}
-          className="font-['vt323'] my-[5rem] -mb-[70px] text-[73px] uppercase tracking-[0.2em]"
+          className="font-['Press_Start_2P'] my-[5rem] -mb-[70px] text-[3.6rem] uppercase tracking-[0.2em]"
         >
           Skill.
         </motion.h1>
 
         <ul className="flex mx-auto gap-4  ">
           {skill.map((result) => (
-            <li className=" ring-2 ring-[#535353]   rounded-[16px] w-[130px]  ">
-              <p className="font-['vt323'] font-bold py-0 text-[2rem]">
+            <li
+              style={{ borderColor: "#535353" }}
+              className=" nes-container   w-[190px] with-title  "
+            >
+              <p className=" title font-['Press_Start_2P']  text-center text-[0.9rem] ">
                 {result.value}%
+              </p>
+              <img className="w-full" src={result.img} alt="js" srcset="" />
+              <p className="font-['Press_Start_2P']  text-center text-[0.9rem] ">
+                {result.name}
               </p>
             </li>
           ))}
         </ul>
       </section>
+
       <section className=" text-[#535353] h-screen w-screen flex flex-col justify-center">
-      <motion.ul className="relative" animate="close" variants={list}>
+        <motion.ul className="relative" animate="close" variants={list}>
           <motion.li
             initial={"close"}
             variants={item}
@@ -262,21 +311,29 @@ function PortoSec() {
             className="bg-cover bg-center w-[60px] h-[140px] absolute bottom-20 right-20"
           />
         </motion.ul>
-      <ul className="flex mx-auto gap-4  ">
+
+        <ul className="flex mx-auto gap-4  ">
           {experience.map((result) => (
-            <li className=" ring-2 ring-[#535353] px-3 py-4  rounded-[16px] w-[340px]  ">
-              <h1 className="font-['vt323'] font-bold text-[2rem]">
+            <li
+              style={{ borderColor: "#535353" }}
+              className=" nes-container with-title  py-4   w-[350px]  "
+            >
+              <p className="title font-['Press_Start_2P']  text-[2rem]">
+                4 Month
+              </p>
+              <h1 className="font-['Press_Start_2P']   text-[0.9rem]">
                 {result.position}({result.as_work_position})
               </h1>
-              <h2 className="font-['vt323'] font-bold -mt-2 text-[1.5rem]">{result.institution}</h2>
-              <h2 className="font-['vt323'] font-medim -mt-2 text-[1.2rem]">{result.duration_job}</h2>
-              <ul className="font-['vt323'] text-[1.3rem] list-disc list-inside text-wrap">
-                 {result.do_work.map((res_do_work)=>
-                <li >
-                 {res_do_work} 
-          
-                  </li>
-                )}
+              <h2 className="font-['Press_Start_2P'] font-bold  text-[0.6rem]">
+                {result.institution}
+              </h2>
+              <h2 className="font-['Press_Start_2P'] font-bold text-[0.5rem]">
+                {result.duration_job}
+              </h2>
+              <ul className="  font-['Press_Start_2P'] text-[0.5rem]  nes-list is-circle">
+                {result.do_work.map((res_do_work) => (
+                  <li className="my-1">{res_do_work}</li>
+                ))}
               </ul>
             </li>
           ))}
@@ -286,13 +343,13 @@ function PortoSec() {
           initial={{ y: 70, opacity: 0 }}
           whileInView={{ y: -70, opacity: 1 }}
           transition={{ ease: "easeInOut", duration: 0.6 }}
-          className="font-['vt323'] text-center my-[5rem] -mb-[70px] text-[73px] uppercase tracking-[0.2em]"
+          className="font-['Press_Start_2P'] text-center my-[5rem] -mb-[70px] text-[3.3rem] uppercase tracking-[0.2em]"
         >
           Experience.
         </motion.h1>
       </section>
       <section className="text-center text-[#535353] h-screen w-screen flex flex-col justify-center">
-      <motion.ul className="relative" animate="close" variants={list}>
+        <motion.ul className="relative" animate="close" variants={list}>
           <motion.li
             initial={"close"}
             variants={item}
@@ -323,40 +380,47 @@ function PortoSec() {
             className="bg-cover bg-center w-[60px] h-[140px] absolute bottom-20 right-20"
           />
         </motion.ul>
-      <ul className="flex mx-auto gap-4  ">
+
+        <ul className="flex mx-auto gap-4  ">
           {experience.map((result) => (
-            <li className=" ring-2 ring-[#535353] px-3 py-4  rounded-[16px] w-[340px]  ">
-              <h1 className="font-['vt323'] font-bold text-[2rem]">
-                {result.position}({result.as_work_position})
-              </h1>
-              <h2 className="font-['vt323'] font-bold -mt-2 text-[1.5rem]">{result.institution}</h2>
-              <h2 className="font-['vt323'] font-medim -mt-2 text-[1.2rem]">{result.duration_job}</h2>
-              <ul className="font-['vt323'] text-[1.3rem] list-disc list-inside text-wrap">
-                 {result.do_work.map((res_do_work)=>
-                <li >
-                 {res_do_work} 
-          
-                  </li>
-                )}
-              </ul>
-            </li>
+            <li
+              style={{ borderColor: "#535353" }}
+              className=" nes-container with-title  py-4   w-[350px]  "
+            ></li>
           ))}
         </ul>
+
         <motion.h1
           initial={{ y: 70, opacity: 0 }}
           whileInView={{ y: -70, opacity: 1 }}
           transition={{ ease: "easeInOut", duration: 0.6 }}
-          className="font-['vt323'] my-[5rem] -mb-[70px] text-[73px] uppercase tracking-[0.2em]"
+          className="font-['Press_Start_2P'] my-[5rem] -mb-[70px] text-[3.3rem] uppercase tracking-[0.2em]"
         >
           Project.{" "}
         </motion.h1>
       </section>
-      <footer className="w-full font-['vt323'] bg-[#535353] h-auto]">
-        <div className="">
-          <h1 className="text-[white]">Surahmat</h1>
+      <footer className="w-full text-[white] py-10 px-40 font-['Press_Start_2P'] bg-[#535353] h-auto]">
+        <div className="w-full ">
+          <h1 className=" text-20 ">Surahmat</h1>
         </div>
+        <div className="flex gap-4">
+          <h2>Follow me: </h2>
+          <div>
+            github
+          </div>
+          <div>
+            linkedin
+          </div>
+          <div>
+            gmail
+          </div>
+          <div>
+            instagram
+          </div>
+        </div>
+        <h1 className="text-[0.8rem]">@Copyright in 2024</h1>
       </footer>
-    </div>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import store from './Ngeredux/store/store';
 import Porto from './Porto/Porto';
 import PortoSec from './Porto/PortoSec';
+import "nes.css/css/nes.min.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
